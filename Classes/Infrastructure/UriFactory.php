aa08ba12e49484991003b1d0867066fe7f2eded6
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-11-11 11:51:57 +0100

    TASK: Refactor factories to infrastructure and introduce UriFactory::createFromDomain

---
+++ Classes/Infrastructure/UriFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeQ\LinkChecker\Infrastructure;
+
+use GuzzleHttp\Psr7\Uri;
+use Neos\Flow\Annotations as Flow;
+use Neos\Neos\Domain\Model\Domain;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class UriFactory
+{
+    public function createFromDomain(Domain $domain): UriInterface
+    {
+        $uri = $domain->getScheme() . '://' . $domain->getHostname()
+            . ($domain->getPort() ? ':' . $domain->getPort() : '');
+        return new Uri($uri);
+    }
+}
