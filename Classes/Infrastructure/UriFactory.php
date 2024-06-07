46d8210323f7333a2a8cc5c2100993f850a0c186
Felix Gradinaru <felix@gradinaru.me>
2024-06-07 15:50:31 +0200

    BUGFIX: Add fallback scheme for UriFactory in case Domain does not have one set

--- Classes/Infrastructure/UriFactory.php
+++ Classes/Infrastructure/UriFactory.php
@@ -16,6 +16,17 @@ class UriFactory
 {
     public function createFromDomain(Domain $domain): UriInterface
     {
-        return new Uri($domain->__toString());
+        $uri = new Uri();
+        $scheme = $domain->getScheme() ?: 'http';
+        $uri = $uri->withScheme($scheme);
+        $uri = $uri->withHost($domain->getHostname());
+        if ($domain->getPort() !== null) {
+            $uri = match ($scheme) {
+                'http' => $uri->withPort($domain->getPort() !== 80 ? $domain->getPort() : null),
+                'https' => $uri->withPort($domain->getPort() !== 443 ? $domain->getPort() : null),
+                default => $uri->withPort($domain->getPort() !== null ? $domain->getPort() : null),
+            };
+        }
+        return $uri;
     }
 }
